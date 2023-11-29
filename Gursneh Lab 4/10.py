d9dc0829c913b110af4c605358e67bd6137b4210
gursneh287 <144039255+gursneh287@users.noreply.github.com>
2023-11-30 00:25:23 +0530

    Update 10.py

--- Gursneh Lab 4/10.py
+++ Gursneh Lab 4/10.py
@@ -4,9 +4,10 @@ c = int(input("enter constant:"))
 D = (b**2)-(4*a*c)
 x1 = (-b+(D**0.5))/2*a
 x2 = (-b-(D**0.5))/2*a
+x = -b/(2*a)
 if D > 0:
     print("x1=",round(x1,2),"x2=",round(x2,2))
 elif D == 0:
-    print("x=",round(x1,2))
+    print("x=",round(x,2))
 elif D<0:
     print("roots are not real")
